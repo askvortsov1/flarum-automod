780efa39f1d6d26fd685302ae927d64461893e7e
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2021-01-16 16:32:14 -0500

    Add trust level routes

--- extend.php
+++ extend.php
@@ -11,14 +11,31 @@
 
 namespace Askvortsov\TrustLevels;
 
+use Askvortsov\TrustLevels\Api\Controller;
 use Flarum\Extend;
+use Flarum\Group\Group;
+use Flarum\User\Event\LoggedIn;
 
 return [
     (new Extend\Frontend('forum'))
-        ->js(__DIR__.'/js/dist/forum.js')
-        ->css(__DIR__.'/resources/less/forum.less'),
+        ->js(__DIR__ . '/js/dist/forum.js')
+        ->css(__DIR__ . '/resources/less/forum.less'),
+
     (new Extend\Frontend('admin'))
-        ->js(__DIR__.'/js/dist/admin.js')
-        ->css(__DIR__.'/resources/less/admin.less'),
+        ->js(__DIR__ . '/js/dist/admin.js')
+        ->css(__DIR__ . '/resources/less/admin.less'),
+
+    (new Extend\Routes('api'))
+        ->get('/trust_levels', 'tags.index', Controller\ListTrustLevelsController::class)
+        ->post('/trust_levels', 'tags.create', Controller\CreateTrustLevelController::class)
+        ->patch('/trust_levels/{id}', 'tags.update', Controller\UpdateTrustLevelController::class)
+        ->delete('/trust_levels/{id}', 'tags.delete', Controller\DeleteTrustLevelController::class),
+
+    (new Extend\Model(Group::class))
+        ->hasMany('trustLevels', TrustLevel::class),
+
+    (new Extend\Model(User::class))
+        ->belongsToMany('trustLevels', TrustLevel::class),
+
     new Extend\Locales(__DIR__ . '/resources/locale')
 ];
