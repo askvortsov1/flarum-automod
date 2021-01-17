74d5a9b707bcdb32a2137c58c86d04df8eaa8c25
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2021-01-17 18:31:53 -0500

    Don't allow editing trust levels (or groups controlled by trust levels) manually

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -1,5 +1,7 @@
 const { default: registerModels } = require("../common/registerModels");
+const { default: augmentEditUserModal } = require("./augmentEditUserModal");
 
 app.initializers.add('askvortsov/flarum-trust-levels', () => {
   registerModels();
+  augmentEditUserModal();
 });
