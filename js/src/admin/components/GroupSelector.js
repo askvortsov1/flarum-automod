f362dad4c7dfa355551fdd7d2487abf76c68aeef
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2021-01-17 15:20:48 -0500

    Improve initial group selection
    - Explain why groups should be deleted when trust levels are deleted
    - Don't show the member group by default

--- js/src/admin/components/GroupSelector.js
+++ js/src/admin/components/GroupSelector.js
@@ -6,21 +6,22 @@ import Group from 'flarum/models/Group';
 
 export default class GroupSelector extends Component {
     view() {
-        const group = app.store.getById('groups', this.attrs.id()) || app.store.getById('groups', Group.MEMBER_ID);
+        const group = app.store.getById('groups', this.attrs.id());
+        const label = group ? [icon(group.icon()), '\t', group.namePlural()] : app.translator.trans('askvortsov-trust-levels.admin.group_selector.placeholder');
         return (
             <div className="Form-group">
                 <label>{this.attrs.label}</label>
 
-                {this.attrs.disabled ? <div className="Button Button--danger">{[icon(group.icon()), '\t', group.namePlural()]}</div>
+                {this.attrs.disabled ? <div className="Button Button--danger">{label}</div>
                 :
-                    <Dropdown label={[icon(group.icon()), '\t', group.namePlural()]} disabled={this.attrs.disabled} buttonClassName="Button Button--danger">
+                    <Dropdown label={label} disabled={this.attrs.disabled} buttonClassName="Button Button--danger">
                         {app.store
                             .all('groups')
                             .filter((g) => ![Group.MEMBER_ID, Group.GUEST_ID].includes(g.id()))
                             .map((g) =>
                                 Button.component(
                                     {
-                                        active: group.id() === g.id(),
+                                        active: group && group.id() === g.id(),
                                         disabled: group && group.id() === g.id(),
                                         icon: g.icon() || icons[g.id()],
                                         onclick: () => {
