d18c24b7bea3a238f9a112b9ad7d1272b0fe62a6
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2023-08-19 14:53:19 -0400

    chore: flarum-cli audit infra --fix
    Signed-off-by: Alexander Skvortsov <sasha.skvortsov109@gmail.com>

---
+++ js/src/common/index.js
@@ -0,0 +1,5 @@
+import app from 'flarum/common/app';
+
+app.initializers.add('askvortsov/flarum-auto-moderator', () => {
+  console.log('[askvortsov/flarum-auto-moderator] Hello, forum and admin!');
+});
